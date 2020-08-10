98e79d21d5b91713208b946371d2ee66d6f9fb55
gabriel <souzatrindade28@gmail.com>
2020-08-10 18:38:58 -0300

    aula

---
+++ variavel.js
@@ -0,0 +1,32 @@
+let variavel = 10;
+variavel = "valor variavel";
+
+console.log(variavel);
+
+
+//escopo global = var
+//escopo local = let
+//const nao muda o valor 
+
+const constante = "valor";
+console.log(constante);
+
+let caseSensitive = 10;
+
+let real = 299.99;
+
+//array(vetor)
+let vetor = [1, 2, 5, "palavra"];
+//console.log(vetor.length);
+console.log(vetor[3])
+
+//ou = ||
+
+//operadores
+console.log(`valor ${2%2}`);//resto da divisão
+console.log(`valor ${3**3}`);//potencia , elevado a
+
+let n = 2;
+n = n + 1 //n++
+n+=2;
+console.log(n);
