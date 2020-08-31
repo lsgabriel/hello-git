b1c5f63256b5fa20d10d1f11e0f2481316d455b3
gabriel <souzatrindade28@gmail.com>
2020-08-31 18:51:51 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+function escreveNaTela(frase){
+    console.log(frase);
+}
+
+escreveNaTela("escreve algo");
+
+const quadrado = function(num){
+    console.log(num*num);
+}
+
+quadrado(2);
+
+const potencia = function(num, potencia){
+    console.log(num**potencia);
+}
+potencia(2, 3);
+
+const soma = function(num1, num2){
+    return num1+num2;
+}
+console.log(soma(1, 3));
